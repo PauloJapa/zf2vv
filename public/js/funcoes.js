76e5f4113682bc8ef5bb2eb452264da8adebab1b
Paulo Watakabe <watakabe05@gmail.com>
2012-12-25 21:02:05 -0200

    Busca Cep Via Ajax

---
+++ public/js/funcoes.js
@@ -0,0 +1,72 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+function retira_acentos(palavra) {
+    com_acento = 'áàãâäéèêëíìîïóòõôöúùûüçÁÀÃÂÄÉÈÊËÍÌÎÏÓÒÕÖÔÚÙÛÜÇ';
+    sem_acento = 'aaaaaeeeeiiiiooooouuuucAAAAAEEEEIIIIOOOOOUUUUC';
+    nova='';
+    for(i=0;i<palavra.length;i++) {
+        if (com_acento.search(palavra.substr(i,1))>=0) {
+            nova+=sem_acento.substr(com_acento.search(palavra.substr(i,1)),1);
+        }else{
+            nova+=palavra.substr(i,1);
+        }
+    }
+    return nova;
+}
+
+function cleanInputsForm(ind){
+    if(ind == null) ind = 0 ;
+    FORM = document.getElementsByTagName('FORM')[ind]
+    for (var i = 0; i < FORM.elements.length; i++) {
+        var obj = FORM.elements[i];
+        cleanInput(obj);
+    }   
+}
+
+function cleanInput(obj){
+    if(!isObject(obj)) obj = document.getElementById(obj) ;
+    switch (obj.type) {
+        case "radio":
+        case "checkbox":
+            obj.checked = false ;
+            break;
+        case "select":
+            obj.selectedIndex = 0 ;            
+        break;
+        case "hidden":
+        case "button":
+            obj.value = obj.value;
+            break;
+        default :
+            obj.value = "" ;
+            break;
+    }   
+}
+//Função para saber se o parametro é objeto
+function isObject( what ){
+    return (typeof what == 'object');
+}
+
+function setSelect(ide,vlr){
+    var Select = document.getElementById(ide);
+    qtd  = Select.options.length ;
+    for (i = 0; i < qtd; i++) {
+	if(Select.options[i].label == vlr){
+            Select.selectedIndex = i;
+            break;
+        }
+    }    
+}
+// verifica se o valor esta no array
+function in_Array(array,vlr){
+    for (key in array) {
+        if (array[key] == vlr) {
+            return true;
+        }
+    }
+    return false ;
+}
